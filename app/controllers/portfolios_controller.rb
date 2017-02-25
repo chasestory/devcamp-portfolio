45dd5d84ca78e0525e1aad71700faeb25e10a541
Chase Story <chasestory@outlook.com>
2017-02-24 23:19:08 -0700

    Integrated Delete feature

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -39,5 +39,17 @@ def edit
       @portfolio_item = Portfolio.find(params[:id])
       end
 
+      def destroy
+        # Perform the look up
+        @portfolio_item = Portfolio.find(params[:id])
+
+      # Destroy/delete the record
+        @portfolio_item.destroy
+
+    # Redirect
+    respond_to do |format|
+      format.html { redirect_to portfolios_url, notice: 'Record was successfully Removed.' }
+          end 
+  end
 
 end
