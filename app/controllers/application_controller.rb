9e3dbe1d64ed203a3b3d6fe30d7dbd5ef9afc9f3
Chase Story <chasestory@outlook.com>
2017-03-08 13:12:59 -0700

    Name Fix

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,18 @@ class ApplicationController < ActionController::Base
   include SetSource
   include CurrentUserConcern
   include DefaultPageContent
+
+  before_action :set_copyright
+
+  def set_copyright
+    @copyright = story_view_tool::Renderer.copyright 'Chase Story', 'All rights reserved'
+  end
+end
+
+module story_view_tool
+  class Renderer
+    def self.copyright name, msg
+      "&copy; #{Time.now.year} | <b>#{name}</b> #{msg}".html_safe
+    end
+  end
 end
