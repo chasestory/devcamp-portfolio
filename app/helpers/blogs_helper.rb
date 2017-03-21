a948aeb138023c25235cfdecb30b0b3df4991f1f
Chase Story <chasestory@outlook.com>
2017-03-21 10:15:38 -0600

    Added default language for heroku in blog helper

--- app/helpers/blogs_helper.rb
+++ app/helpers/blogs_helper.rb
@@ -3,3 +3,9 @@ module BlogsHelper
     image_tag "https://www.gravatar.com/avatar/#{Digest::MD5.hexdigest(user.email)}", width: 60
   end
 end
+
+class CodeRayify < Redcarpet::Render::HTML
+  def block_code(code, language)
+    CodeRay.scan(code, language ||= :ruby).div
+  end
+end 
