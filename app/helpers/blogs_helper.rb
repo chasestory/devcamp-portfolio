9d390f4197e693d10044e2fb81ad3310cb37a06c
Chase Story <chasestory@outlook.com>
2017-03-21 10:43:02 -0600

    removed class from blogs_helper

--- app/helpers/blogs_helper.rb
+++ app/helpers/blogs_helper.rb
@@ -3,9 +3,3 @@ module BlogsHelper
     image_tag "https://www.gravatar.com/avatar/#{Digest::MD5.hexdigest(user.email)}", width: 60
   end
 end
-
-class CodeRayify < Redcarpet::Render::HTML
-  def block_code(code, language)
-    CodeRay.scan(code, language ||= :ruby).div
-  end
-end 
