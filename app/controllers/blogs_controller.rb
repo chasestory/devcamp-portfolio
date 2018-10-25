92260a34786982ad717e8e9efaa62814c0d388c0
chasestory@outlook.com <chasestory@me.com>
2018-10-24 19:36:06 -0600

    added topic sidebar fix

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -12,7 +12,6 @@ class BlogsController < ApplicationController
     else
       @blogs = Blog.published.recent.page(params[:page]).per(5)
     end
-    @page_title = "My Portfolio Blog"
   end
 
   # GET /blogs/1
