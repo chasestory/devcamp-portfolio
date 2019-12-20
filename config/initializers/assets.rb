979f79da9805eaea8c1f83b89420e292fb5358c9
chasestory <chasestory@outlook.com>
2019-12-20 16:40:57 -0700

    upgrade app potential breakage app wide

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -3,18 +3,10 @@
 # Version of your assets, change this if you want to expire all your assets.
 Rails.application.config.assets.version = '1.0'
 
-# Add additional assets to the asset load path
+# Add additional assets to the asset load path.
 # Rails.application.config.assets.paths << Emoji.images_path
-Rails.application.config.assets.paths << Rails.root.join('node_modules')
-# Precompile additional assets.
 
-# application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-# Rails.application.config.assets.precompile += %w( search.js )
-Rails.application.config.assets.precompile += %w( blogs.scss )
-Rails.application.config.assets.precompile += %w( portfolios.scss )
-Rails.application.config.assets.precompile += %w( application.scss )
-Rails.application.config.assets.precompile += %w( register.scss )
-Rails.application.config.assets.precompile += %w( application.js )
-Rails.application.config.assets.precompile += %w( html.sortable.js )
-Rails.application.config.assets.precompile += %w( comments.coffee )
-Rails.application.config.assets.precompile += %w( blogs.coffee )
+# Precompile additional assets.
+# application.js, application.css, and all non-JS/CSS in the app/assets
+# folder are already added.
+# Rails.application.config.assets.precompile += %w( admin.js admin.css )
